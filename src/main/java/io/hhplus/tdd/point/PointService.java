446558cee4721a296643c9d845c4ac98ae2da078
sunggyupaik <melon9751@naver.com>
2025-10-16 00:20:32 +0900

    포인트 조회 서비스를 추가하라
    주어진 사용자 식별자로 포인트 조회를 추가합니다.

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,15 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PointService {
+    private final UserPointTable userPointTable;
+
+    public UserPoint point(long id) {
+        return userPointTable.selectById(id);
+    }
+}
