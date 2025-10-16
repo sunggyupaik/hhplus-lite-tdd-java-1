dc5a97fc026c7a87fbd8f2cd76fe85e85306345c
sunggyupaik <melon9751@naver.com>
2025-10-16 23:38:39 +0900

    포인트 내역 조회 서비스를 추가하라
    주어진 사용자 식별자에 해당하는 포인트 내역 조회 서비스를 추가합니다

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,15 +1,19 @@
 package io.hhplus.tdd.point;
 
 import io.hhplus.tdd.common.response.ErrorCode;
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.common.exception.BaseException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PointService {
     private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
 
     public UserPoint point(long id) {
         if (id <= 0L) {
@@ -17,5 +21,13 @@ public class PointService {
         }
 
         return userPointTable.selectById(id);
+    }
+
+    public List<PointHistory> history(long id) {
+        if (id <= 0L) {
+            throw new BaseException(ErrorCode.USER_NOT_FOUND);
+        }
+
+        return pointHistoryTable.selectAllByUserId(id);
     }
 }
