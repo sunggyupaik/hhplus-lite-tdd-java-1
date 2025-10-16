e6d8ef7444d87661c5d1d21cdb94eed26c635849
sunggyupaik <melon9751@naver.com>
2025-10-16 23:17:56 +0900

    포인트 조회 서비스 유효성 검사 테스트를 추가하라
    주어진 사용자 식별자가 유효하지 않으면 예외를 던지는 테스트를 추가합니다.

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.common.exception.BaseException;
 import io.hhplus.tdd.database.UserPointTable;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -41,6 +43,19 @@ class PointServiceTest {
                 UserPoint userPoint = pointService.point(EMPTY_USER_ID);
 
                 Assertions.assertEquals(userPoint.point(), 0L);
+            }
+        }
+
+        @Nested
+        @DisplayName("만약 유효하지 않은 사용자 식별자가 주어진다면")
+        class Context_with_invalid_user_id {
+            private static final long INVALID_USER_ID = -1L;
+
+            @Test
+            @DisplayName("유효하지 않은 식별자라는 예외를 리턴한다")
+            void it_throws_invalid_user_id() {
+                assertThatThrownBy(() -> pointService.point(INVALID_USER_ID))
+                        .isInstanceOf(BaseException.class);
             }
         }
     }
