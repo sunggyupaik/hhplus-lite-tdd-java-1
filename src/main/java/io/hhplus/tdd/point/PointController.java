362b56d142572ff47b85fb9d65c804baf7ec6677
sunggyupaik <melon9751@naver.com>
2025-10-16 23:53:52 +0900

    포인트 내역 조회 요청을 추가하라
    주어진 사용자 식별자에 해당하는 포인트 내역 조회 요청을 추가합니다

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -21,14 +21,11 @@ public class PointController {
         return pointService.point(id);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
-     */
     @GetMapping("{id}/histories")
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
+        return pointService.history(id);
     }
 
     /**
