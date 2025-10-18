4b43f54e622ff0c34221f31b643cca8be4edac3f
sunggyupaik <melon9751@naver.com>
2025-10-18 18:14:43 +0900

    포인트 사용 서비스 테스트를 추가하라
    주어진 사용자 식별자와 금액으로 사용 테스트를 추가합니다.
    사용 후 0원 미만이거나 100원 미만을 사용하면 예외를 던집니다.

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -165,7 +165,7 @@ class PointServiceTest {
             @Test
             @DisplayName("존재하지 않는 식별자라는 예외를 리턴한다")
             void it_throws_not_existed_user_id_exception() {
-                assertThatThrownBy(() -> pointService.history(NOT_EXISTED_USER_ID))
+                assertThatThrownBy(() -> pointService.charge(NOT_EXISTED_USER_ID, AMOUNT_3000))
                         .isInstanceOf(BaseException.class);
             }
         }
@@ -188,6 +188,78 @@ class PointServiceTest {
             @DisplayName("최소 100원 이상을 충전해야한다는 예외를 던진다")
             void it_throws_less_than_100_point_exception() {
                 assertThatThrownBy(() -> pointService.charge(EXISTED_USER_ID, AMOUNT_50))
+                        .isInstanceOf(BaseException.class);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("use 메서드는")
+    class Describe_of_use {
+        private static final long EXISTED_USER_ID = 1L;
+        private static final long AMOUNT_50 = 50L;
+        private static final long AMOUNT_1000 = 1000L;
+        private static final long AMOUNT_3000 = 3000L;
+        private static final long AMOUNT_4000 = 4000L;
+        private static final long AMOUNT_100000 = 100000L;
+
+        UserPoint initUserPoint = new UserPoint(EXISTED_USER_ID, AMOUNT_4000, System.currentTimeMillis());
+        UserPoint leftUserPoint = new UserPoint(EXISTED_USER_ID, AMOUNT_1000, System.currentTimeMillis());
+
+        @BeforeEach
+        void prepare() {
+            when(userPointTable.selectById(EXISTED_USER_ID)).thenReturn(initUserPoint);
+            when(userPointTable.insertOrUpdate(EXISTED_USER_ID, AMOUNT_3000)).thenReturn(leftUserPoint);
+        }
+
+        @Nested
+        @DisplayName("만약 존재하는 사용자 식별자와 충전금액이 주어진다면")
+        class Context_with_existed_user_id_and_amount {
+            @Test
+            @DisplayName("기존 금액에서 요청 금액을 뺀 유저포인트를 리턴한다")
+            void it_returns_left_user_point_after_use() {
+                UserPoint userPoint = pointService.use(EXISTED_USER_ID, AMOUNT_1000);
+                System.out.println(userPoint);
+
+                Assertions.assertEquals(userPoint.point(), AMOUNT_1000,
+                        "기존 4000원에 요청금액 3000원을 뺀 1000원을 리턴한다");
+
+                verify(userPointTable, times(1)).selectById(EXISTED_USER_ID);
+                verify(userPointTable, times(1)).insertOrUpdate(EXISTED_USER_ID, AMOUNT_3000);
+            }
+        }
+
+        @Nested
+        @DisplayName("만약 존재하지 않은 사용자 식별자가 주어진다면")
+        class Context_with_not_existed_user_id {
+            private static final long NOT_EXISTED_USER_ID = -1L;
+
+            @Test
+            @DisplayName("존재하지 않는 식별자라는 예외를 리턴한다")
+            void it_throws_not_existed_user_id_exception() {
+                assertThatThrownBy(() -> pointService.use(NOT_EXISTED_USER_ID, AMOUNT_3000))
+                        .isInstanceOf(BaseException.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("만약 사용 후 금액이 0원 미만이라면")
+        class Context_with_left_point_less_than_zero {
+            @Test
+            @DisplayName("포인트 잔액은 0보다 커야한다는 예외를 던진다")
+            void it_throws_over_using_possible_amount_exception() {
+                assertThatThrownBy(() -> pointService.use(EXISTED_USER_ID, AMOUNT_100000))
+                        .isInstanceOf(BaseException.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("만약 100원 미만의 금액을 사용한다면")
+        class Context_with_less_than_100_point {
+            @Test
+            @DisplayName("최소 100원 이상을 사용해야한다는 예외를 던진다")
+            void it_throws_less_than_100_point_exception() {
+                assertThatThrownBy(() -> pointService.use(EXISTED_USER_ID, AMOUNT_50))
                         .isInstanceOf(BaseException.class);
             }
         }
